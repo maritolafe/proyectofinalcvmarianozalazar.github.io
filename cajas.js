f18fd8f5eb0f59dcd41299315bf2aa0cd4fb3d4b
Mariano <103077649+maritolafe@users.noreply.github.com>
2023-02-24 16:58:57 -0300

    cvproyectofinal
    argentina4.0

---
+++ cajas.js
@@ -0,0 +1,21 @@
+
+// funcion para ocultar la caja de menu del encabezado
+function mostrar(){
+var lista =document.getElementById('lista');
+if(lista.style.display =='none'){
+lista.style.display ='block'
+
+
+}else{
+lista.style.display ='none'
+}
+}
+
+ 
+// funcion menu de encabezado
+function cartel(){
+var encima=document.getElementsByClassName('elemento');
+
+alert("ir al espacio de la web");
+}
+// funacion para angrandar los logos
